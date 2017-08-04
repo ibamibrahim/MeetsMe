e44621a7454cc7a4acae90511ea2b18b1186fa93
Ibam <ibamibrahim0@gmail.com>
2017-08-04 11:36:52 +0700

    initial commit, add login screen skeleton

---
+++ app/src/main/java/id/meetsme/meetsme/LandingPageActivity.java
@@ -0,0 +1,13 @@
+package id.meetsme.meetsme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandingPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+}
